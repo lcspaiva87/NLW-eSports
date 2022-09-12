9bbfea19d2307852ddd5bd9cf85c397864b49c2a
lucas paiva <lucas.paiva@mundiale.com.br>
2022-09-12 15:30:45 -0300

    fix: de bug styles

--- src/globals/styles/global.ts
+++ src/globals/styles/global.ts
@@ -4,39 +4,38 @@ export const theme = extendTheme({
     config: {
         intialColorMode: "dark",
         useSystemColorMode: true,
-      },
+    },
     styles: {
         global: {
             body: {
                 fontFamily:
-                    "sans-serif,Inter ", 
+                    "sans-serif,Inter ",
                 WebKitFontSmoothing: "antialiased",
                 MozOsxFontSmoothing: "grayscale",
                 margin: 0,
                 padding: 0,
-                boxSizing:' border-box',
-                bgImage:"linear-gradient(rgba(99, 59, 188, 1), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0),rgb(0, 0, 0)) ,url(/nevoa.png),url(/estrelas.png) ",
-                //background:'',
-                color:"#fff"
-              
+                boxSizing: ' border-box',
+                bgImage:"linear-gradient(rgb(99 59 188 / 13%), rgba(0,0,0)), url(/nevoa.png),url(/estrelas.png) ",  
+                color: "#fff"
+
             },
 
             code: {
                 fontFamily:
-                "sans-serif,Inter ", 
-                
+                    "sans-serif,Inter ",
+
             },
             colors: {
                 brand: {
                     input: '#18181B',
-                    violet:"#8B5CF6",
-                    default:"#71717A",
-                    modal:'#2A2634',
-                    gradient:{
-                        gren:'rgba(67, 231, 173, 1)',
-                        purple:',rgba(149, 114, 252, 1)',
-                        yellow:'rgba(225, 213, 93, 1)',
-                        white:'rgba(255, 255, 255, 1)'
+                    violet: "#8B5CF6",
+                    default: "#71717A",
+                    modal: '#2A2634',
+                    gradient: {
+                        gren: 'rgba(67, 231, 173, 1)',
+                        purple: ',rgba(149, 114, 252, 1)',
+                        yellow: 'rgba(225, 213, 93, 1)',
+                        white: 'rgba(255, 255, 255, 1)'
                     }
                 },
             },
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider} from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { theme } from '../globals/styles/global'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    
       <Component {...pageProps} />
     </ChakraProvider>
   )
